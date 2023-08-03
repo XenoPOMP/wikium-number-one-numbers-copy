21c1079f9654d532d9c5e76386c40711dd230168
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-08-03 18:07:46 +0300

    подготовил шаблон для главной страницы

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import cn from 'classnames';
+
+import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
+
 export default function Home() {
-  return <main>index page</main>;
+  return (
+    <main className={cn('py-[1.6rem]')}>
+      <UiContainer className={cn('flex justify-center items-center')}>
+        index page
+      </UiContainer>
+    </main>
+  );
 }
