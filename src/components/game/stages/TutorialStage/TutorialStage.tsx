ff6a0b7798faca0a52c07d001feff66f6599c9b0
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-08-07 01:23:30 +0300

    добавил градиентную картинку

--- src/components/game/stages/TutorialStage/TutorialStage.tsx
+++ src/components/game/stages/TutorialStage/TutorialStage.tsx
@@ -1,7 +1,8 @@
 import cn from 'classnames';
-import { FC } from 'react';
+import { CSSProperties, FC } from 'react';
 
 import { AppConstants } from '@/app/app.constants';
+import numbersImage from '@/public/numbers-image-1.svg';
 
 import styles from './TutorialStage.module.scss';
 import type { TutorialStageProps } from './TutorialStage.props';
@@ -9,16 +10,32 @@ import type { TutorialStageProps } from './TutorialStage.props';
 const TutorialStage: FC<TutorialStageProps> = ({}) => {
   const { width, height } = AppConstants.gameSizeConstraints;
 
+  const getInlineStyles = (): Record<'article' | 'heading', CSSProperties> => {
+    const output: ReturnType<typeof getInlineStyles> = {
+      article: {},
+      heading: {},
+    };
+
+    output.article = {
+      width: `min(100%, ${width}px)`,
+      height,
+      border: '2px solid red',
+    };
+
+    output.heading = {
+      // background: `${numbersImage.src}`,
+      // backgroundSize: 'cover',
+    };
+
+    return output;
+  };
+
   return (
-    <div
-      style={{
-        width: `min(100%, ${width}px)`,
-        height,
-        border: '2px solid red',
-      }}
-    >
-      Tutorial
-    </div>
+    <article className={cn(styles.tutorial)} style={getInlineStyles().article}>
+      <div className={cn(styles.heading)} style={getInlineStyles().heading}>
+        <h1>Header</h1>
+      </div>
+    </article>
   );
 };
 
